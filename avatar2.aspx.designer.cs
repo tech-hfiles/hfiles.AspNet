e513d7e1a0437f514c641208a38a1f38a1d1c484
Ramkamlesh31 <kamleshram562@gmail.com>
2025-01-13 13:41:08 +0530

    updated avatar2.aspx

--- avatar2.aspx.designer.cs
+++ avatar2.aspx.designer.cs
@@ -87,15 +87,6 @@ namespace hfiles
         protected global::System.Web.UI.WebControls.Label lbluser;
 
         /// <summary>
-        /// LinkButton4 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton LinkButton4;
-
-        /// <summary>
         /// LinkButton1 control.
         /// </summary>
         /// <remarks>
@@ -103,6 +94,15 @@ namespace hfiles
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.LinkButton LinkButton1;
+
+        /// <summary>
+        /// LinkButton4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.LinkButton LinkButton4;
 
         /// <summary>
         /// storageused control.
